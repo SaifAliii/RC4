84019800fa53542f0c303c7072cd30337845229d
saifAlii <s.a.sunny19156@gmail.com>
2023-01-31 17:14:14 +0500

    implement the Rc4

--- main.cpp
+++ main.cpp
@@ -30,66 +30,17 @@ void keyScheduling()
 	int j = 0;
 	for (int i = 0; i < 256; i++)
 	{
-		j = (j + S[i] + (K[i] - '0')) % 256;
+		j = (j + S[i] + K[i]) % 256;
 		swap(S[i], S[j]);
 	}
 
 }
-string reverseString(string str)
+
+string encryptionDecryption(string text,string key)
 {
-	int length = str.length();
-	int endingIndex;
-	char temp;
-	if (length % 2 == 0)
-	{
-		endingIndex = length - 1;
-	}
-	else
-	{
-		endingIndex = length;
-	}
-	for (int i = 0; i < length / 2; i++, endingIndex--)
-	{
-		temp = str[i];
-		str[i] = str[endingIndex];
-		str[endingIndex] = temp;
-	}
-
-	return str;
-}
-
-string convertIntoBinary(int val)
-{
-	string binary;
-	int remainder;
-	if (val == 0)
-	{
-		return "00";
-	}
-	while (val != 0)
-	{
-		remainder = val % 2;
-		if (remainder == 0)
-		{
-			binary += '0';
-		}
-		else
-		{
-			binary += '1';
-		}
-		val = val / 2;
-	}
-	if (binary.length() < 2)
-	{
-		binary = '0' + binary;
-	}
-
-	binary = reverseString(binary);
-	return binary;
-}
-
-string encryptionDecryption(string text)
-{
+	initializeS_Array();
+	initializeK_Array(key);
+	keyScheduling();
 	string keyStream;
 	int i = 0;
 	int j = 0;
@@ -102,9 +53,8 @@ string encryptionDecryption(string text)
 		j = (j + S[i]) % 256;
 		swap(S[i], S[j]);
 		t = (S[i] + S[j]) % 256;
-		keyStream[k] = S[t];
-		kStream = convertIntoBinary(keyStream[k]);
-		resultingText += (text[k] ^ keyStream) + '0';
+		keyStream += S[t];
+		resultingText += text[k] ^ S[t];
 	}
 	return resultingText;
 }
@@ -117,14 +67,9 @@ int main()
 	string plainText, cipherText, decryptedText;
 	cout << "Enter the plain text: ";
 	cin >> plainText;
-	initializeS_Array();
-	initializeK_Array(key);
-	keyScheduling();
-	cipherText = encryptionDecryption(plainText);
+	cipherText = encryptionDecryption(plainText,key);
 	cout << "cipher Text is: " << cipherText << endl;
-	decryptedText = encryptionDecryption(cipherText);
+	decryptedText = encryptionDecryption(cipherText,key);
 	cout << "decrypted text is: " << decryptedText << endl;
-	
-
 	return 0;
 }
